d9693650aa347bf93d12f01ce52d6ce58c8eca03
Sejal Kotian <73776092+SejalKotian@users.noreply.github.com>
2021-07-17 21:05:00 +0530

    Delete forms.py
    not used

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired, Length, Email
-class Queryform(FlaskForm):
-    username=StringField('UserName',validators=[DataRequired(),Length (min=2,max=20)])
-    email= StringField('Email',validators=[DataRequired(),Email()])
-    message=StringField('Message',validators=[DataRequired(),Length(min=2,max=50)])
-    submit=SubmitField('Submit')
-
